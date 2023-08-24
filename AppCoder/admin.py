094d586cd8d4e12a12fea6386cc98252b11c3488
Mauro Alberelli <mauroalberelli@gmail.com>
2023-08-24 18:33:48 -0300

    second commit casa

--- AppCoder/admin.py
+++ AppCoder/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Usuario)
+admin.site.register(Peliculas)
+admin.site.register(Series)
+admin.site.register(Directores)
