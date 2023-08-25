48c7afbb5e3806e6edb1d32c8c51f4165f87a604
Mauro Alberelli <mauroalberelli@gmail.com>
2023-08-24 21:39:54 -0300

    se agrego un for para imagenes y se modifico la base de datos

--- AppCoder/admin.py
+++ AppCoder/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Usuario)
+
 admin.site.register(Peliculas)
 admin.site.register(Series)
 admin.site.register(Directores)
+admin.site.register(Generos)
+admin.site.register(Curadores)
--- AppCoder/models.py
+++ AppCoder/models.py
@@ -1,27 +1,53 @@
 from django.db import models
 
-class Usuario(models.Model):
-    nombre=models.CharField(max_length=50)
-    apeliido=models.CharField(max_length=50)
-    email=models.EmailField()
-    preferencias=models.CharField(max_length=50)
-
-class Peliculas(models.Model):
-
-    Titulo =models.CharField(max_length=50)
-    Genero= models.CharField(max_length=50)
-    año=models.IntegerField()
-    duracion=models.CharField(max_length=10)
-
-class Series(models.Model):
-
-    Titulo =models.CharField(max_length=50)
-    Genero= models.CharField(max_length=50)
-    Temporadas= models.IntegerField()
-    año=models.IntegerField()
-    
 
 class Directores(models.Model):
     nombre =models.CharField(max_length=50)
     apeliido=models.CharField(max_length=50)
-    filmografia=models.CharField(max_length=50)
+    filmografia=models.ManyToManyField('Peliculas',blank=True)
+
+    def __str__(self):
+        return f'{self.nombre} {self.apeliido}'
+    
+class Curadores(models.Model):
+    nombre =models.CharField(max_length=50)
+    apeliido=models.CharField(max_length=50)
+    reseñas=models.ManyToManyField('Peliculas',blank=True)
+
+    def __str__(self):
+        return f'{self.nombre} {self.apeliido}'
+
+
+class Peliculas(models.Model):
+
+    titulo =models.CharField(max_length=50)
+    slug = models.SlugField(unique=True,null=True)
+    overview = models.TextField(null=True)
+    genero= models.ManyToManyField('Generos')
+    año=models.IntegerField()
+    director = models.ForeignKey(Directores,on_delete=models.CASCADE,null=True)
+    reseña = models.TextField(null=True)
+    autor_reseña= models.ForeignKey(Curadores,on_delete=models.CASCADE,null=True)
+    cover = models.ImageField(null=True)
+    imagen = models.ImageField(null=True)
+
+    def __str__(self):
+        return self.titulo
+
+class Series(models.Model):
+
+    titulo =models.CharField(max_length=50)
+    genero= models.CharField(max_length=50)
+    temporadas= models.IntegerField()
+    año=models.IntegerField()
+
+
+    
+class Generos(models.Model):
+    genero=models.CharField(max_length=50)
+    slug = models.SlugField(unique=False)
+
+    def __str__(self):
+        return self.genero
+
+
